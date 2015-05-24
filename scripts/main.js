ea307f414001af4734770a9dbab447ca18902cbe
Jesse Millar <hellojessemillar@gmail.com>
2015-05-24 17:30:19 -0600

    Testing scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 var init = function() {
-    parseURL();
+    console.log(parseURL());
 };
 
 var parseURL = function() {
     var url = window.location.href.split('/');
 
-    console.log(url);
+    return url[3];
 };
