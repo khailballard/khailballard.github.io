1cd0116cd818122e777a31f93ba3e8f9ce768184
Jesse Millar <hellojessemillar@gmail.com>
2015-05-24 18:27:28 -0600

    Adding functionality

--- scripts/main.js
+++ scripts/main.js
@@ -8,18 +8,22 @@ var init = function() {
         document.getElementById('arrow_left').style.opacity = 0;
         document.getElementById('arrow_right').style.opacity = 0;
     } else {
-        if (parseInt(current) > 1) {
-            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(current) - 1) + "/";
-        } else {
-            document.getElementById('arrow_left').style.opacity = 0.5;
-        }
+        if (current) {
+            if (parseInt(current) > 1) {
+                document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(current) - 1) + "/";
+            } else {
+                document.getElementById('arrow_left').style.opacity = 0.5;
+            }
 
-        if (parseInt(current) < parseInt(latest)) {
-            document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (parseInt(current) + 1) + "/";
+            if (parseInt(current) < parseInt(latest)) {
+                document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (parseInt(current) + 1) + "/";
+            } else {
+                document.getElementById('arrow_right').style.opacity = 0.5;
+            }
         } else {
+            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(latest) - 1) + "/";
             document.getElementById('arrow_right').style.opacity = 0.5;
         }
-        
     }
 };
 
