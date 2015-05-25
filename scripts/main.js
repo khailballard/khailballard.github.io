70a478dbd26894af3c837fd42b4a22b2a217f146
Jesse Millar <hellojessemillar@gmail.com>
2015-05-24 18:26:19 -0600

    Whoops

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,13 @@ var init = function() {
         document.getElementById('arrow_right').style.opacity = 0;
     } else {
         if (parseInt(current) > 1) {
-            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(param) - 1) + "/";
+            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(current) - 1) + "/";
         } else {
             document.getElementById('arrow_left').style.opacity = 0.5;
         }
 
         if (parseInt(current) < parseInt(latest)) {
-            document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (parseInt(param) + 1) + "/";
+            document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (parseInt(current) + 1) + "/";
         } else {
             document.getElementById('arrow_right').style.opacity = 0.5;
         }
