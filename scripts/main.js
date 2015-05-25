3397289853a77a4cfde94cb9bc8f99887d46cb0d
Jesse Millar <hellojessemillar@gmail.com>
2015-05-25 14:18:13 -0600

    Fixing arrow opacity on non-post pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,30 @@
 var init = function() {
-    var current = parseURL();
-    var latest = document.getElementById('latestPostNumber').innerHTML;
-
-    if (current == 'archive') {
-        document.getElementById('arrow_left_image').style.opacity = 0;
-        document.getElementById('arrow_right_image').style.opacity = 0;
-    } else {
-        if (current.length) {
-            if (parseInt(current) > 1) {
-                document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(current) - 1) + "/";
-            } else {
-                document.getElementById('arrow_left_image').style.opacity = 0.5;
-            }
-
-            if (parseInt(current) < parseInt(latest)) {
-                document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (parseInt(current) + 1) + "/";
-            } else {
-                document.getElementById('arrow_right_image').style.opacity = 0.5;
-            }
-        } else {
-            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (parseInt(latest) - 1) + "/";
-            document.getElementById('arrow_right_image').style.opacity = 0.5;
-        }
-    }
-};
-
-var parseURL = function() {
     var url = window.location.href.split('/');
 
-    return url[3];
+    var current = parseInt(url[3]);
+    var latest = parseInt(document.getElementById('latestPostNumber').innerHTML);
+
+    var disabledOpacity = 0.25;
+
+    if (current > 0) {
+        document.getElementById('arrow_left_image').style.opacity = 1;
+        document.getElementById('arrow_right_image').style.opacity = 1;
+
+        if (current > 1) {
+            document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (current - 1) + "/";
+        } else {
+            document.getElementById('arrow_left_image').style.opacity = disabledOpacity;
+        }
+
+        if (current < latest) {
+            document.getElementById('arrow_right').href = "http://www.reconditewoods.com/" + (current + 1) + "/";
+        } else {
+            document.getElementById('arrow_right_image').style.opacity = disabledOpacity;
+        }
+    } else {
+        document.getElementById('arrow_left_image').style.opacity = 1;
+        
+        document.getElementById('arrow_left').href = "http://www.reconditewoods.com/" + (latest - 1) + "/";
+        document.getElementById('arrow_right_image').style.opacity = disabledOpacity;
+    }
 };
